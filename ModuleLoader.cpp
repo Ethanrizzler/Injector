1f8fcfb35895d403573982e87b9f4e96a8c6f2f1
Ethan <ethanondablock786@gmail.com>
2025-03-24 05:25:10 +0100

    ModuleLoader.cpp

--- ModuleLoader.cpp
+++ ModuleLoader.cpp
@@ -13,7 +13,8 @@
 #define PAGE_EXECUTE_READ 0x20
 #define WH_GETMESSAGE 3
 #define WM_NULL 0x0000
-DWORD GetProcessId(const std::wstring& processName)
+
+DWORD GetProcessId(const std::wstring& processName)  
 {
     DWORD ret = 0;
     HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -25,7 +26,7 @@ DWORD GetProcessId(const std::wstring& processName)
         {
             do
             {
-                if (processName.compare(entry.szExeFile) == 0)
+                if (processName.compare(entry.szExeFile) == 0)  
                 {
                     ret = entry.th32ProcessID;
                     break;
@@ -37,67 +38,93 @@ DWORD GetProcessId(const std::wstring& processName)
     return ret;
 }
 
-bool InjectDLL(HANDLE processHandle, const std::string& dllPath) {
-    LPVOID allocMem = VirtualAllocEx(processHandle, NULL, dllPath.size() + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    if (!allocMem) {
-        std::cerr << "[!] Failed to allocate memory in target process." << std::endl;
-        return false;
-    }
-
-    if (!WriteProcessMemory(processHandle, allocMem, dllPath.c_str(), dllPath.size() + 1, NULL)) {
-        std::cerr << "[!] Failed to write DLL path to target process memory." << std::endl;
-        return false;
-    }
-
-    HMODULE hKernel32 = GetModuleHandle(L"kernel32.dll");
-    if (!hKernel32) {
-        std::cerr << "[!] Failed to get handle for kernel32.dll." << std::endl;
-        return false;
-    }
-
-    LPTHREAD_START_ROUTINE loadLibraryAddr = (LPTHREAD_START_ROUTINE)GetProcAddress(hKernel32, "LoadLibraryA");
-    if (!loadLibraryAddr) {
-        std::cerr << "[!] Failed to get address of LoadLibraryA." << std::endl;
-        return false;
-    }
-
-    HANDLE remoteThread = CreateRemoteThread(processHandle, NULL, 0, loadLibraryAddr, allocMem, 0, NULL);
-    if (!remoteThread) {
-        std::cerr << "[!] Failed to create remote thread." << std::endl;
-        return false;
-    }
-
-    WaitForSingleObject(remoteThread, INFINITE);
-    CloseHandle(remoteThread);
-    return true;
-}
-
 int main()
 {
-    std::cout << "[+] Waiting for Roblox process..." << std::endl;
+    SetConsoleTitle(L"Injector");
 
-    DWORD processId;
-    while (!(processId = GetProcessId(L"RobloxPlayerBeta.exe"))) {
+    std::cout << "Waiting for Roblox process..." << std::endl;
+
+    HWND windowHandle;
+    while (true)
+    {
+        windowHandle = FindWindow(NULL, L"Roblox");  
+        if (IsWindowVisible(windowHandle))
+            break;
+
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
-    std::cout << "[+] PID: " << processId << std::endl;
+    system("cls");
 
-    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-    if (!processHandle) {
-        std::cerr << "[!] Failed to open Roblox process." << std::endl;
+    DWORD processId = GetProcessId(L"RobloxPlayerBeta.exe");
+    if (processId == 0)
+    {
+        std::cout << "Failed to find Roblox process." << std::endl;
         return -1;
     }
 
-    std::string dllPath = "C:\\Users\\Bobby\\source\\repos\\ModuleLoader\\x64\\Debug\\ModuleX012.dll";
-
-    if (InjectDLL(processHandle, dllPath)) {
-        std::cout << "[+] Module Loaded" << std::endl;
-    }
-    else {
-        std::cerr << "[!] DLL injection failed." << std::endl;
+    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
+    if (!processHandle)
+    {
+        std::cout << "Failed to open process." << std::endl;
+        return -1;
     }
 
+    // PATCH WinVerifyTrust  
+    HMODULE wintrustModule = LoadLibraryA("wintrust.dll");
+    FARPROC _winVerifyTrust = GetProcAddress(wintrustModule, "WinVerifyTrust");
+
+    BYTE payload[] = { 0x48, 0x31, 0xC0, 0x59, 0xFF, 0xE1 }; 
+
+    DWORD oldProtect;
+    if (!VirtualProtectEx(processHandle, _winVerifyTrust, sizeof(payload), PAGE_EXECUTE_READWRITE, &oldProtect))
+        std::cout << "Failed to protect WinVerifyTrust." << std::endl;
+
+    SIZE_T bytesWritten;
+    if (!WriteProcessMemory(processHandle, _winVerifyTrust, payload, sizeof(payload), &bytesWritten))
+        std::cout << "Failed to patch WinVerifyTrust." << std::endl;
+
+    VirtualProtectEx(processHandle, _winVerifyTrust, sizeof(payload), PAGE_EXECUTE_READ, &oldProtect);
+
+    //  DLL INJECTION THROUGH LoadLibraryA  
+    if (GetFileAttributesA("Module.dll") == INVALID_FILE_ATTRIBUTES)
+    {
+        std::cout << "DLL not found." << std::endl;
+        return -1;
+    }
+
+    LPVOID allocatedMem = VirtualAllocEx(processHandle, NULL, MAX_PATH, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (!allocatedMem)
+    {
+        std::cout << "Failed to allocate memory in target process." << std::endl;
+        return -1;
+    }
+
+    if (!WriteProcessMemory(processHandle, allocatedMem, "nyx.dll", strlen("nyx.dll") + 1, NULL))
+    {
+        std::cout << "Failed to write DLL path to process memory." << std::endl;
+        return -1;
+    }
+
+    LPVOID loadLibraryAddr = (LPVOID)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
+    if (!loadLibraryAddr)
+    {
+        std::cout << "Failed to get LoadLibraryA address." << std::endl;
+        return -1;
+    }
+
+    HANDLE remoteThread = CreateRemoteThread(processHandle, NULL, 0, (LPTHREAD_START_ROUTINE)loadLibraryAddr, allocatedMem, 0, NULL);
+    if (!remoteThread)
+    {
+        std::cout << "Failed to create remote thread." << std::endl;
+        return -1;
+    }
+
+    std::cout << "Module Attached Successfully." << std::endl;
+    
+    CloseHandle(remoteThread);
     CloseHandle(processHandle);
+
+    std::this_thread::sleep_for(std::chrono::hours(999));  
     return 0;
 }
